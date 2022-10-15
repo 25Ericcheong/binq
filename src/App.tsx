fe1aafd55d265e78752a0aaf03ae7b9fb9a4d0ae
25Ericcheong <ericcheongkaikit@gmail.com>
2022-10-15 16:14:10 +1000

    realized header was missing - fixed it with z-index

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div id="app">
-      {renderImages()}
+      {/* {renderImages()} */}
       <section id="header">
         <Header menuClicked={menuClicked} setMenuClicked={setMenuClicked} />
       </section>
