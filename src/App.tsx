696e7756ac3aa518fcc7c5556c5bcc3965af2153
25Ericcheong <ericcheongkaikit@gmail.com>
2022-10-15 13:55:18 +1000

    changed images for my liking

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ function App() {
   const [sliderIndex, setSliderIndex] = useState(0);
 
   const data = [
-      "/assets/image/1-kyoto-fushimi-inari.jpg",
-      "/assets/image/2-tokyo-alley.jpg", "/assets/image/3-sensoji-temple.jpg", "/assets/image/4-osaka-castle.jpg", "/assets/image/5-himeji-castle.jpg"
+      "/assets/image/1-tokyo-skytree.jpg",
+      "/assets/image/2-tokyo-alley.jpg", "/assets/image/3-shibuya-crossing.jpg", "/assets/image/4-tokyo-bridge.jpg", "/assets/image/5-tokyo-city.jpg"
   ]
 
   function renderImages() {
-    return data.map((path, imageRenderIndex) => { return <img src={path} id="image-positioning" alt="These are used as background decorations" className={sliderIndex === imageRenderIndex ? 'show-image' : 'hide-image'} />
+    return data.map((path, imageRenderIndex) => { return <img src={path} id="image-positioning" alt="Missing data should be rendered here in the background" className={sliderIndex === imageRenderIndex ? 'show-image' : 'hide-image'} />
     })
   }
 
