1bb4a6c234fdebad86eca950916748c0c355d3ba
25Ericcheong <ericcheongkaikit@gmail.com>
2023-01-20 09:24:49 +1000

    component separation

---
+++ src/BackgounrdImages/BackgroundImages.tsx
@@ -0,0 +1,19 @@
+import "../App.scss";
+
+export const BackgounrdImages = () => {
+  // function renderBackgroundImages() {
+  //   updateImagePosition();
+  //   return imageData.map((img, imageDataIndex) => {
+  //     const imageVisbility = determineImgVisbility(imageDataIndex);
+  //     const style = `${imageFullSizeClass} ${imageVisbility}`;
+  //     return (
+  //       <img
+  //         className={style}
+  //         key={imageDataIndex}
+  //         src={img.path}
+  //         alt={img.alt}
+  //       />
+  //     );
+  //   });
+  // }
+};
