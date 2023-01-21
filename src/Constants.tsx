3d33697e6fc578ad141c161801a7719580690da0
25Ericcheong <ericcheongkaikit@gmail.com>
2023-01-21 18:05:03 +0800

    added style class constants

---
+++ src/Constants.tsx
@@ -0,0 +1,18 @@
+export const STYLE = {
+  selected: "selected",
+  notSelected: "not-selected",
+};
+
+export const STYLE_IMAGE = {
+  toShow: {
+    fullSize: "image-fullsize",
+    image: "image-show",
+  },
+
+  toAnimate: {
+    showFromUp: "image-show-from-up",
+    showFromDown: "image-show-from-down",
+    hideToUp: "image-hide-up",
+    hideToDown: "image-hide-down",
+  },
+};
