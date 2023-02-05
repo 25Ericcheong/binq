5f8baeb0e185c2a648554b109a85ad63b523aac5
25Ericcheong <ericcheongkaikit@gmail.com>
2023-02-05 21:50:22 +1000

    missed out some code styling to segregate and added icon that will hide/show footer cards

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 import "../App.scss";
 import "./Footer.scss";
 
+import { HiOutlineArrowCircleRight } from "react-icons/hi";
 import IMAGE_DATA from "../Data.json";
+import { IconContext } from "react-icons/lib/esm/iconContext";
 import { STYLE } from "../Constants";
 
 interface Props {
@@ -29,9 +31,20 @@ export const Footer = ({ imageIndex }: Props) => {
     });
   }
 
+  function renderButton() {
+    return (
+      <IconContext.Provider value={{ size: "2em" }}>
+        <div className="icon-footer">
+          <HiOutlineArrowCircleRight />
+        </div>
+      </IconContext.Provider>
+    );
+  }
+
   return (
     <footer className="order-body-part-footer mark medium-spacing">
       {renderCards()}
+      {renderButton()}
     </footer>
   );
 };
