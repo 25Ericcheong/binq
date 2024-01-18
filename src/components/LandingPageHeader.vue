8688a558164b08faa9524fcf567d0f04d09ece62
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-18 21:05:54 +0800

    feature: missed commits for some files

---
+++ src/components/LandingPageHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Child from "./Child.vue";
+
+</script>
+
+<template>
+  <Header class="bg-blue-50 font-bold">
+    <div>
+    <h1 >BINQ BINQ BINQ</h1>
+  </div>
+  </Header>
+  <Child />
+</template>
+
+<style scoped>
+</style>
