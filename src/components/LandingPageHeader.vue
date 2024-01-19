081af9fe278295401cd3a8b5afb54d4a025bb71a
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-19 18:08:28 +0800

    feat: Created initial design of top bar that will be sticky through web and right side to be used as menu later on if keen

--- src/components/LandingPageHeader.vue
+++ src/components/LandingPageHeader.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import Child from "./Child.vue";
-
+import TopBarHeader from "./TopBarHeader.vue";
 </script>
 
 <template>
-  <Header class="bg-blue-50 font-bold">
+  <header class="bg-blue-50 font-bold">
+    <TopBarHeader/>
     <div>
-    <h1 >BINQ BINQ BINQ</h1>
-  </div>
-  </Header>
+      <h1 >BINQ BINQ BINQ</h1>
+    </div>
+  </header>
   <Child />
 </template>
 
---
+++ src/components/TopBarHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="grid grid-cols-10 grid-rows-1 h-20">
+    <div class="col-span-9 flex justify-center">
+      <img class="object-contain" src="../assets/logo/binq.png">
+    </div>
+    <div class="text-center flex items-center">
+      <p>ICE</p>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+</style>
