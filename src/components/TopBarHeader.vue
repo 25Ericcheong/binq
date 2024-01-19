9c8c84ab1c59682f4fd096628af28608eaed61f9
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-19 18:25:07 +0800

    feat: Alternative to sizing image with padding to prevent over crowding and totally removed unnecessary white spaces around original image

--- src/components/TopBarHeader.vue
+++ src/components/TopBarHeader.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <nav class="grid grid-cols-10 grid-rows-1 h-20">
-    <div class="col-span-9 flex justify-center">
+  <nav class="grid grid-cols-10 grid-rows-1 h-15 py-3">
+    <div class="col-span-9 flex justify-center h-8">
       <img class="object-contain" src="../assets/logo/binq.png">
     </div>
-    <div class="text-center flex items-center">
+    <div class="text-center flex items-center justify-center">
       <p>ICE</p>
     </div>
   </nav>
