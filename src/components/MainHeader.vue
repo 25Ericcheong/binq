87ca56f1cf5e358de48e55b777bd34d622f1835c
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-21 17:37:22 +0800

    feat: Added easy to remember slogan and added additional spacing

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -4,12 +4,17 @@ const TITLE = "Binq";
 
 <template>
   <section class="flex justify-between">
-    <div class="w-1/3">
-      <div class="ml-6 md:text-9xl sm:text-6xl header-font" v-for="n in 3">
-        <h1>{{ TITLE }}</h1>
+    <div class="w-1/2">
+      <div class="ml-10" v-for="n in 3">
+        <h1 class="md:text-9xl sm:text-6xl header-font">{{ TITLE }}</h1>
+      </div>
+      <div class="ml-10 mt-4 body-font">
+        <p>
+          A selection of flavorful homemade Binq ice - Crave More, It's So Nice!
+        </p>
       </div>
     </div>
-    <div class="w-1/3">
+    <div class="w-1/3 mr-10">
       <img src="../assets/pictures/roasted-soybean-oolong-bingsu.png" />
     </div>
   </section>
