cca1d9e62095d849c809e9f02032a55b1a19051e
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-23 23:18:20 +0800

    feat: Additional buttons to have a look at top 3 best seller bing su

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -3,10 +3,10 @@ const TITLE = "Binq";
 </script>
 
 <template>
-  <section class="flex max-w-full max-h-screen primary-bg py-8">
+  <section class="flex max-h-screen primary-bg py-12">
     <div class="w-5/12">
       <div class="ml-20 text-6xl sm:text-3xl body-font pb-4">
-        <p>A selection of unique homemade</p>
+        <p>A selection of unique refreshing</p>
         <button
           class="border-solid border-2 text-whitebq border-b-greenbq p-3 rounded-full quarternary-bg"
         >
@@ -17,14 +17,31 @@ const TITLE = "Binq";
         <h1 class="md:text-9xl sm:text-6xl header-font">{{ TITLE }}</h1>
       </div>
     </div>
-    <div class="mr-10">
-      <div
-        class="items-start secondary-bg p-14 rounded-full border-solid border-2 border-b-greenbq"
-      >
-        <img
-          class=""
-          src="../assets/pictures/roasted-soybean-oolong-bingsu.png"
-        />
+    <div class="mr-10 flex flex-col h-full">
+      <div class="flex h-3/4 justify-center">
+        <div
+          class="items-start secondary-bg p-3 rounded-full border-solid border-2 border-b-greenbq"
+        >
+          <img
+            class="h-full"
+            src="../assets/pictures/roasted-soybean-oolong-bingsu.png"
+          />
+        </div>
+      </div>
+      <div class="flex body-font mt-4">
+        <button
+          class="border-solid border-2 text-whitebq border-b-greenbq p-3 rounded-full quarternary-bg"
+        >
+          Soybean Oolong</button
+        ><button
+          class="flex-1 border-solid border-2 text-whitebq border-b-greenbq p-3 rounded-full quarternary-bg"
+        >
+          Matcha</button
+        ><button
+          class="flex-1 border-solid border-2 text-whitebq border-b-greenbq p-3 rounded-full quarternary-bg"
+        >
+          Hoji Cha
+        </button>
       </div>
     </div>
   </section>
