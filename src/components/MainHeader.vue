1cc7b57f6c35401308886f9d1dfb31440bec5eaa
25Ericcheong <ericcheongkaikit@gmail.com>
2024-01-27 11:39:01 +0800

    fix: Improved responsiveness for large monitor. Still need to improve laptop and mobile bit

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -4,9 +4,9 @@ const TITLE = "Binq";
 
 <template>
   <section
-    class="flex flex-col sm:flex-row sm:justify-between sm:py-44 sm:px-48 bg-gradient-to-tr from-darkorangebq from-15% via-orangebq via-40% to-dullorangebq overflow-x-hidden"
+    class="flex flex-col sm:flex-row sm:justify-between bg-gradient-to-tr from-darkorangebq from-15% via-orangebq via-40% to-dullorangebq overflow-x-hidden"
   >
-    <div class="sm:w-1/3">
+    <div class="sm:w-1/3 sm:py-44 sm:px-48 flex flex-col justify-center">
       <div class="text-6xl sm:text-3xl body-font mb-10">
         <h2 class="text-whitebq pb-3">A <i>mountain</i> of delicious</h2>
         <button
@@ -23,41 +23,25 @@ const TITLE = "Binq";
         </h1>
       </div>
     </div>
-    <div class="flex h-full justify-center sm:w-1/2 relative">
-      <div class="flex-col">
-        <div class="flex justify-center">
-          <div
-            class="flex items-start secondary-bg p-10 rounded-full drop-shadow-2xl tertiary-bg"
-          >
-            <img
-              class="h-full w-full"
-              src="../assets/pictures/roasted-soybean-oolong-bingsu.png"
-            />
-          </div>
-          <div
-            class="p-2 max-w-80 rounded-lg drop-shadow-2xl absolute top-0 left-60 text-xl backdrop-blur-lg bg-darkorangebq bg-opacity-30 text-whitebq"
-          >
-            <h2 class="header-font font-semibold mb-2">Best Seller</h2>
-            <p class="body-font">Soybean Oolong</p>
-          </div>
-          <div
-            class="text-xl p-2 rounded-lg drop-shadow-2xl absolute bottom-20 right-44 max-w-40 backdrop-blur-lg bg-darkorangebq bg-opacity-30 text-whitebq"
-          >
-            <h2 class="mb-2 header-font font-semibold">Sides</h2>
-            <p class="mt-2 body-font">
-              Sweet jelly, Kinako cream cheese mochi and buttery crisp
-            </p>
-          </div>
+    <div class="sm:w-1/2 flex flex-col sm:pr-48 sm:py-28">
+      <div class="m-10">
+        <div
+          class="flex justify-center rounded-full drop-shadow-2xl tertiary-bg"
+        >
+          <img
+            class="h-5/6 w-5/6"
+            src="../assets/pictures/roasted-soybean-oolong-bingsu.png"
+          />
         </div>
-        <div class="flex justify-evenly body-font mt-5 text-darkorangebq">
-          <button class="mr-3 p-3 tertiary-bg font-black rounded-lg">
-            Soybean Oolong</button
-          ><button class="flex-1 mr-3 tertiary-bg font-medium rounded-lg">
-            Matcha</button
-          ><button class="flex-1 tertiary-bg p-3 font-medium rounded-lg">
-            Hoji Cha
-          </button>
-        </div>
+      </div>
+      <div class="flex justify-evenly body-font mt-5 text-darkorangebq">
+        <button class="flex-1 mr-3 p-3 tertiary-bg font-black rounded-lg">
+          Soybean Oolong</button
+        ><button class="flex-1 mr-3 tertiary-bg font-medium rounded-lg">
+          Matcha</button
+        ><button class="flex-1 tertiary-bg p-3 font-medium rounded-lg">
+          Hoji Cha
+        </button>
       </div>
     </div>
   </section>
