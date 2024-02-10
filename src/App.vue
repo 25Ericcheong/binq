e93bf887be5a9523a4e48370aa0429bbb5d04f76
25Ericcheong <ericcheongkaikit@gmail.com>
2024-02-10 11:02:35 +0800

    chore: Renamed pics and move to different directories - will still need to update paths in files

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import LandingPageHeader from "./components/header/LandingPageHeader.vue";
+import TopBar from "./components/TopBar.vue";
 import "./output.css";
 </script>
 
 <template>
   <div>
+    <TopBar />
     <LandingPageHeader />
   </div>
 </template>
--- src/components/header/LandingPageHeader.vue
+++ src/components/header/LandingPageHeader.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import MainHeader from "./MainHeader.vue";
-import TopBarHeader from "./TopBarHeader.vue";
 </script>
 
 <template>
   <header class="max-h-screen">
-    <TopBarHeader />
     <MainHeader />
   </header>
 </template>
