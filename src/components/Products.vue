77081b3dac8fdfb14ad1bf5e0cb7cf8a975b1736
25Ericcheong <ericcheongkaikit@gmail.com>
2024-03-01 22:27:11 +0800

    feat: Modify directory name and am able to also render images of bingsu only now

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,70 +2,112 @@
 import { ref } from "vue";
 
 enum Group {
-  BinqSet = "BINQSET",
-  Binq = "BINQ",
+  BingsuSet = "BINQSET",
+  Bingsu = "BINQ",
   Topping = "TOPPING",
 }
 
 type Product = {
   name: string;
   image: string;
-  sides?: string[];
-  group: Group;
+  toppings?: string[];
+  groupType: Group;
 };
 
 const products = ref<Product[]>([
   {
     name: "Roasted Soybean Oolong",
+    image: "roasted-soybean-oolong-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "White Peach",
+    image: "white-peach-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "Matcha",
+    image: "matcha-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "Hojicha",
+    image: "hojicha-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "Kumquat Jasmine",
+    image: "kumquat-jasmine-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "Dark Knight",
+    image: "dark-knight-bingsu-removebg-preview",
+    groupType: Group.Bingsu,
+  },
+  {
+    name: "Roasted Soybean Oolong",
     image: "roasted-soybean-oolong-removebg-preview",
-    sides: ["Brown Sugar Jelly", "Soybean-Coated Mochi Cube", "Butter Crisp"],
-    group: Group.BinqSet,
-  },
-  {
-    name: "White Peach Oolong",
-    image: "white-peach-oolong-removebg-preview",
-    sides: ["Brown Sugar Jelly", "Peach Popping Boba", "Taro Ball"],
-    group: Group.BinqSet,
-  },
-  {
-    name: "The Dark Knight",
-    image: "the-dark-knight-removebg-preview",
-    sides: ["White Sugar Jelly", "Milo Coated Mochi Cube", "Butter Crisp"],
-    group: Group.BinqSet,
+    toppings: [
+      "Brown Sugar Jelly",
+      "Soybean-Coated Mochi Cube",
+      "Butter Crisp",
+    ],
+    groupType: Group.BingsuSet,
   },
   {
     name: "Matcha",
     image: "matcha-removebg-preview",
-    sides: [
+    toppings: [
       "Brown Sugar Jelly",
       "Soybean-Coated Mochi Cube",
       "Red Bean",
       "Cream Cheese",
     ],
-    group: Group.BinqSet,
-  },
-  {
-    name: "Kumquat Jasmine",
-    image: "kumquat-jasmine-removebg-preview",
-    sides: ["Passionfruit Popping Boba", "Nata De Coco", "Aiyu Jelly"],
-    group: Group.BinqSet,
+    groupType: Group.BingsuSet,
   },
   {
     name: "Hoji Cha Cha",
     image: "hojicha-cha-removebg-preview",
-    sides: [
+    toppings: [
       "Brown Sugar Jelly",
       "Soybean-Coated Mochi Cube",
       "Red Bean",
       "Cream Cheese",
     ],
-    group: Group.BinqSet,
+    groupType: Group.BingsuSet,
+  },
+  {
+    name: "White Peach Oolong",
+    image: "white-peach-oolong-removebg-preview",
+    toppings: ["Brown Sugar Jelly", "Peach Popping Boba", "Taro Ball"],
+    groupType: Group.BingsuSet,
+  },
+  {
+    name: "The Dark Knight",
+    image: "the-dark-knight-removebg-preview",
+    toppings: ["White Sugar Jelly", "Milo Coated Mochi Cube", "Butter Crisp"],
+    groupType: Group.BingsuSet,
+  },
+  {
+    name: "Kumquat Jasmine",
+    image: "kumquat-jasmine-removebg-preview",
+    toppings: ["Passionfruit Popping Boba", "Nata De Coco", "Aiyu Jelly"],
+    groupType: Group.BingsuSet,
   },
 ]);
 
-function getImageUrl(name: string) {
+function getImageUrl(name: string, groupType: Group) {
+  let productInnerDir = "bingsu-set";
+
+  if (groupType === Group.Bingsu) {
+    productInnerDir = "bingsu";
+  } else if (groupType === Group.Topping) {
+    productInnerDir = "topping";
+  }
+
   return new URL(
-    `/src/assets/images/products/bingsu-set/${name}.png`,
+    `/src/assets/images/products/${productInnerDir}/${name}.png`,
     import.meta.url
   ).href;
 }
@@ -75,10 +117,10 @@ function getImageUrl(name: string) {
   <section></section>
   <section>
     <div v-for="product in products">
-      <img :src="getImageUrl(product.image)" />
+      <img :src="getImageUrl(product.image, product.groupType)" />
       <h2>{{ product.name }}</h2>
-      <div v-if="product.sides !== undefined">
-        <p v-for="side in product.sides">{{ side }}</p>
+      <div v-if="product.toppings !== undefined">
+        <p v-for="topping in product.toppings">{{ topping }}</p>
       </div>
     </div>
   </section>
