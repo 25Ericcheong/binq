2fc6ee8712c040c134f24876cb33669d814a623e
25Ericcheong <ericcheongkaikit@gmail.com>
2024-03-16 10:48:02 +0800

    feat: Halfway done with first seciton of product intro info

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -9,9 +9,9 @@ function getImageUrl(imageName: string, productDir: string) {
 
 <template>
   <div
-    class="my-72 bg-gradient-to-tl from-darkorangebq from-5% via-orangebq via-50% to-dullorangebq py-40"
+    class="my-72 bg-gradient-to-tl from-darkorangebq from-5% via-orangebq via-50% to-dullorangebq py-40 px-40 xxl:px-64"
   >
-    <section class="flex mx-64">
+    <section class="flex">
       <div class="text-creamwhitebq">
         <h1
           class="text-7xl xl:text-8xl xxl:text-10xl header-font font-extrabold pb-3"
@@ -19,48 +19,36 @@ function getImageUrl(imageName: string, productDir: string) {
           Discover Binq
         </h1>
         <h2
-          class="text-2xl lg:text-2xl xl:text-3xl xxl:text-5xl body-font mb-10 text-creamwhitebq"
+          class="text-2xl xl:text-3xl xxl:text-5xl body-font mb-10 text-creamwhitebq"
         >
           With an assortment of flavors available, the combination is limitless
         </h2>
       </div>
     </section>
-    <section class="flex pt-40 mx-64">
+    <section class="flex pt-16 xl:pt-28 xxl:pt-40">
       <div class="flex text-creamwhitebq">
-        <div class="mr-64">
-          <h2
-            class="text-2xl lg:text-2xl xl:text-3xl xxl:text-5xl body-font mb-10"
-          >
+        <div class="flex flex-col mr-20 xl:mr-40 xxl:mr-64">
+          <h2 class="text-2xl xl:text-3xl xxl:text-5xl body-font mb-10">
             For all to have the best experience, we take preparation and taste
             to the next level;
           </h2>
-          <p
-            class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl mb-48"
-          >
+          <p class="body-font text-xl xxl:text-2xl mb-20 xxl:mb-48">
             At Binq, we not only put countless of hours into researching the
             best combination of flavors out there but we want to ensure each and
             every single order is made to perfection for all to enjoy.
           </p>
-          <div class="flex justify-between">
-            <img
-              class="h-1/2 w-1/2"
-              :src="getImageUrl('prep-bingsu-product-intro', 'intro')"
-            />
-            <img
-              class="h-1/4 w-1/2 self-center"
-              :src="getImageUrl('more-prep-bing-product-intro', 'intro')"
-            />
-          </div>
           <img
-            class="my-40"
+            class="h-1/4"
+            :src="getImageUrl('more-prep-bing-product-intro', 'intro')"
+          />
+          <img
+            class="my-20 xxl:my-40"
             :src="getImageUrl('topping-product-intro', 'intro')"
           />
-          <div class="flex justify-between">
-            <img
-              class="rounded-full"
-              :src="getImageUrl('lychee-topping-product-intro', 'intro')"
-            />
-          </div>
+          <img
+            class="rounded-full w-4/5 h-1/4 self-center"
+            :src="getImageUrl('lychee-topping-product-intro', 'intro')"
+          />
         </div>
         <img
           :src="getImageUrl('bingsus-product-intro', 'intro')"
@@ -68,7 +56,7 @@ function getImageUrl(imageName: string, productDir: string) {
         />
       </div>
     </section>
-    <section class="flex pt-80 mx-64 justify-between">
+    <section class="flex mt-80 justify-between">
       <div class="flex flex-col w-2/5">
         <div
           class="bg-creamyellowbq p-10 rounded-lg text-darkorangebq flex-col"
