5c3a6115a0a98b154693fb92befac71181f239db
25Ericcheong <ericcheongkaikit@gmail.com>
2024-03-18 22:28:44 +0800

    feat: Styling to content to be more readable to some extend

--- src/components/OurMentions.vue
+++ src/components/OurMentions.vue
@@ -7,7 +7,7 @@ import { getImageUrl } from "@/util/Image.js";
     class="bg-creamyellowbq w-full h-full py-32 lg:py-72 px-12 sm:px-14 md:px-20"
   >
     <section class="text-darkorangebq flex">
-      <div class="w-1/3">
+      <div class="w-1/4">
         <h1
           class="text-5xl sm:text-7xl xl:text-8xl xxl:text-10xl header-font font-extrabold pb-3"
         >
@@ -20,36 +20,35 @@ import { getImageUrl } from "@/util/Image.js";
         </h2>
       </div>
       <img
-        class="w-2/3 pl-20"
+        class="w-3/4 pl-20"
         :src="getImageUrl('bingsu-our-mentions', 'our-mentions')"
       />
     </section>
-    <section class="flex items-end w-2/3">
-      <div class="flex flex-col">
+    <section class="flex py-20">
+      <div class="flex flex-col w-1/3 pr-32">
         <div
-          class="flex flex-row justify-between text-xl xl:text-2xl xxl:text-4xl body-font mb-5"
+          class="flex flex-row justify-between text-xl xl:text-2xl xxl:text-2xl body-font mb-5"
         >
           <h2>"11/10 Can confidently recommend this to anyone"</h2>
         </div>
         <p
-          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide h-1/3 overflow-hidden"
         >
-          11/10 Can confidently recommend this to anyone, especially to all the
-          bingsu lovers out there. They&apos;ve got several choices of flavour
-          which none are too sweet so it&apos;s also a perfect place for the
-          older audience as well. Service wise, super friendly staff especially
-          the young couple which are actually the owners of the place! Very
-          attentive and are also super helpful to give suggestions if
-          you&apos;re not sure what to try. Bingsu wise: Hoji Cha 10/10 - As a
-          hojicha lover this hit the spot perfectly, with the alternative
-          oatmilk option it was super rich in flavour but also balanced so it
-          wasn&apos;t too much. Roasted Soybean 10/10 - Slightly a bit on the
-          sweeter side but balanced out perfectly with the cream cheese. Matcha
-          9/10 - Full of matcha flavour on every taste, would definitely satisfy
-          the matcha lovers. Generally a great spot to visit with friends and
-          family for dessert after a meal. Have heard nothing but complements
-          when bringing people to try for the first time (and like me becoming
-          regulars after)
+          Especially to all the bingsu lovers out there. They&apos;ve got
+          several choices of flavour which none are too sweet so it&apos;s also
+          a perfect place for the older audience as well. Service wise, super
+          friendly staff especially the young couple which are actually the
+          owners of the place! Very attentive and are also super helpful to give
+          suggestions if you&apos;re not sure what to try. Bingsu wise: Hoji Cha
+          10/10 - As a hojicha lover this hit the spot perfectly, with the
+          alternative oatmilk option it was super rich in flavour but also
+          balanced so it wasn&apos;t too much. Roasted Soybean 10/10 - Slightly
+          a bit on the sweeter side but balanced out perfectly with the cream
+          cheese. Matcha 9/10 - Full of matcha flavour on every taste, would
+          definitely satisfy the matcha lovers. Generally a great spot to visit
+          with friends and family for dessert after a meal. Have heard nothing
+          but complements when bringing people to try for the first time (and
+          like me becoming regulars after)
         </p>
         <div
           class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
@@ -58,15 +57,14 @@ import { getImageUrl } from "@/util/Image.js";
           <p>Source</p>
         </div>
       </div>
-      <div class="flex flex-col">
-        <div class="text-xl xl:text-2xl xxl:text-4xl body-font mb-5">
+      <div class="flex flex-col w-1/3 pr-32">
+        <div class="text-xl xl:text-2xl xxl:text-2xl body-font mb-5">
           <h2>
-            "Overall a perfect hangout place with ur friends and family with
-            good dessert!"
+            "Perfect hangout place with friends & family with good dessert!"
           </h2>
         </div>
         <p
-          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide h-1/3 overflow-hidden"
         >
           Love their hojicha bingsu!!! By far the richest taste I&apos;ve tried!
           And every time I come here this is a confirmed repeat order. (This is
@@ -85,12 +83,12 @@ import { getImageUrl } from "@/util/Image.js";
           <p>Source</p>
         </div>
       </div>
-      <div class="flex flex-col">
-        <div class="text-xl xl:text-2xl xxl:text-4xl body-font mb-5">
+      <div class="flex flex-col w-1/3">
+        <div class="text-xl xl:text-2xl xxl:text-2xl body-font mb-5">
           <h2>"Flavors were different than other bingsu places"</h2>
         </div>
         <p
-          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide h-1/3 overflow-hidden"
         >
           Tried Binq for the first time and had a pleasant experience. Flavors
           were different than other bingsu places, worth trying because so far
