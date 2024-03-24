0a567deb720cc377d9c38e3599706f60293cc015
25Ericcheong <ericcheongkaikit@gmail.com>
2024-03-24 09:49:44 +0800

    feat: About to finish off our mentions section with a few touch ups left to do

--- src/components/OurMentions.vue
+++ src/components/OurMentions.vue
@@ -4,9 +4,9 @@ import { getImageUrl } from "@/util/Image.js";
 
 <template>
   <div
-    class="bg-creamyellowbq w-full h-full py-32 lg:py-72 px-12 sm:px-14 md:px-20"
+    class="bg-creamyellowbq w-full py-32 lg:py-72 px-12 sm:px-14 md:px-20 text-darkorangebq"
   >
-    <section class="text-darkorangebq flex">
+    <section class="flex">
       <div class="w-1/4">
         <h1
           class="text-5xl sm:text-7xl xl:text-8xl xxl:text-10xl header-font font-extrabold pb-3"
@@ -24,8 +24,8 @@ import { getImageUrl } from "@/util/Image.js";
         :src="getImageUrl('bingsu-our-mentions', 'our-mentions')"
       />
     </section>
-    <section class="flex py-20">
-      <div class="flex flex-col w-1/3 pr-32">
+    <section class="flex justify-between py-20 font-medium">
+      <div class="flex flex-col w-1/4 flex-shrink-0">
         <div
           class="flex flex-row justify-between text-xl xl:text-2xl xxl:text-2xl body-font mb-5"
         >
@@ -51,13 +51,19 @@ import { getImageUrl } from "@/util/Image.js";
           like me becoming regulars after)
         </p>
         <div
-          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide mt-5"
         >
-          <p>Source</p>
+          <a
+            class="p-2 rounded-lg text-creamwhitebq bg-darkorangebq"
+            href="https://maps.app.goo.gl/GbsjxS6GdFXhUJso8"
+            target="_blank"
+            rel="noreferrer"
+            >More</a
+          >
           <p>J K - 12/03/2024</p>
         </div>
       </div>
-      <div class="flex flex-col w-1/3 pr-32">
+      <div class="flex flex-col w-1/4">
         <div class="text-xl xl:text-2xl xxl:text-2xl body-font mb-5">
           <h2>
             "Perfect hangout place with friends & family with good dessert!"
@@ -68,22 +74,28 @@ import { getImageUrl } from "@/util/Image.js";
         >
           Love their hojicha bingsu!!! By far the richest taste I&apos;ve tried!
           And every time I come here this is a confirmed repeat order. (This is
-          my 4th time here already🙈) I also like their - oolong peach (imagine
-          a chagee but shaved ice version 😍) - soybean (you will love it if u
-          like injeolmi) - matcha Each bingsu comes with 3-4 toppings. U can
-          also modify ur toppings. But I think the combination is already good
-          so I never modify. Environment is chill and cozy, also provide
-          drinking water. Overall a perfect hangout place with ur friends and
-          family with good dessert!
+          my 4th time here already) I also like their - oolong peach (imagine a
+          chagee but shaved ice version) - soybean (you will love it if u like
+          injeolmi) - matcha Each bingsu comes with 3-4 toppings. U can also
+          modify ur toppings. But I think the combination is already good so I
+          never modify. Environment is chill and cozy, also provide drinking
+          water. Overall a perfect hangout place with ur friends and family with
+          good dessert!
         </p>
         <div
-          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide mt-5"
         >
-          <p>Source</p>
+          <a
+            class="p-2 rounded-lg text-creamwhitebq bg-darkorangebq"
+            href="https://maps.app.goo.gl/c8iAMvf23wuFSoBN6"
+            target="_blank"
+            rel="noreferrer"
+            >More</a
+          >
           <p>Kathryn Chong - 18/01/2024</p>
         </div>
       </div>
-      <div class="flex flex-col w-1/3">
+      <div class="flex flex-col w-1/4">
         <div class="text-xl xl:text-2xl xxl:text-2xl body-font mb-5">
           <h2>"Flavors were different than other bingsu places"</h2>
         </div>
@@ -110,12 +122,67 @@ import { getImageUrl } from "@/util/Image.js";
           back again.
         </p>
         <div
-          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide"
+          class="flex flex-row justify-between body-font text-xl sm:text-2xl lg:text-sm xl:text-xl xxl:text-2xl tracking-wide mt-5"
         >
-          <p>Source</p>
+          <a
+            class="p-2 rounded-lg text-creamwhitebq bg-darkorangebq"
+            href="https://maps.app.goo.gl/RCc2zopeopnzvsfq9"
+            target="_blank"
+            rel="noreferrer"
+            >More</a
+          >
           <p>Lizzie - 18/01/2024</p>
         </div>
       </div>
     </section>
+    <section class="flex">
+      <div class="w-1/2">
+        <h1
+          class="text-5xl sm:text-7xl xl:text-8xl xxl:text-10xl header-font font-extrabold"
+        >
+          Buzzworthy Binq moments
+        </h1>
+      </div>
+      <div class="w-1/2 scroller">
+        <ul>
+          <li>
+            Several choices of flavour which are not too sweet - making them
+            perfect
+          </li>
+          <li>
+            Hoji Cha 10/10 - As a hijocha lover; this hit the spot perfectly
+          </li>
+          <li>
+            My 4th time being here and I love their oolong peach, soybean and
+            matcha
+          </li>
+          <li>Roasted Soybean 10/10 - Slightly sweeter but balanced</li>
+          <li>
+            My first time trying Binq and flavors were unique and different;
+            making them refreshing!
+          </li>
+          <li>Matcha 9/10 - Full of matcha flavour in every bite</li>
+          <li>Roasted soybean oolong paired really well with cream cheese</li>
+          <li>
+            Perhaps one of the best bingsu in town in terms of that fine
+            snowflake like texture
+          </li>
+          <li>
+            Shaved ice was light and soybean mochi cube was springy and nicely
+            paired with lots of brown sugar jelly
+          </li>
+          <li>One of the most delicious bingsu I’ve tried</li>
+          <li>Their soya bean coated mochi cubes were absolutely delish!</li>
+          <li>Roasted soybean oolong 4.75 out of 5 on flavour alone!</li>
+          <li>My favourite will be their signature oolong and soya</li>
+          <li>
+            Tried their roasted soybean oolong and so satisfied with it. It has
+            perfect fragrant, taste, and texture
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
+
+<style scoped></style>
