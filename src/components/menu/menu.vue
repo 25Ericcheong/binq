427729914c788b97d6e2e2adb696c4f0528c2b2b
25Ericcheong <ericcheongkaikit@gmail.com>
2024-05-06 22:18:58 +0800

    feat: Added header to menu and low resolution images to project

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -2,6 +2,14 @@
 import "../../output.css";
 </script>
 <template>
-  <section class="bg-creamwhitebq"></section>
+  <section
+    class="bg-gradient-to-tr from-darkorangebq from-15% via-orangebq via-25% to-dullorangebq"
+  >
+    <h1
+      class="p-5 text-7xl xl:text-8xl header-font font-extrabold text-creamwhitebq"
+    >
+      Binq Menu
+    </h1>
+  </section>
 </template>
 <style scoped></style>
